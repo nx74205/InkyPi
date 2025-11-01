ba88d2f5e81b8d37a1802035ac2e936339d2c247
nx74205 <kl.volmer@gmx.de>
2025-11-01 09:28:35 +0000

    Progress

--- src/plugins/solaredge/solaredge.py
+++ src/plugins/solaredge/solaredge.py
@@ -46,29 +46,58 @@ class Solaredge(BasePlugin):
         return image
 
     def parse_solar_data(self):
-        cdt = datetime.now()
-        batteryLevel = 50
-        consumptionToday = 4.5
-        productionToday = 6.82
-        currentPower = 1256
 
-        data = {            
-            "battery_png": self.get_plugin_dir(f'icons/{'battery-50'}.png'),
+        DECIMAL_SIGN = ","
+
+        def replace_decimals(s1: str) -> str:
+            if not isinstance(s1, str) :
+                return s1
+            return s1.replace(".", DECIMAL_SIGN)
+
+        cdt = datetime.now()
+
+        batteryLevel = round(45, 0)
+        batteryChargeAmount = 1256
+        batteryCapacity = 9700
+
+        solarMaxPower = 5500
+        solarProductionToday = 6.82
+        solarCurrentPower = 2300
+
+        consumptionToday = 4.5
+        
+        currentSolarPower = 2500
+
+        data = {
             "solaredge_png": self.get_plugin_dir(f'icons/solaredge.png'),
-            "solar_house_png": self.get_plugin_dir(f'icons/solarhaus.png'),
+            "battery": {
+                "icon": self.get_plugin_dir(f'icons/battery-' + f'{int(round(batteryLevel / 10) * 10)}' + '.png'),
+                "level": str(batteryLevel) + " %",
+                "capacity": replace_decimals(str(round(batteryCapacity/1000, 1)) + " kWh"),
+                "current_power": replace_decimals(str(round(batteryChargeAmount/1000, 1)) + " kWh")
+            },
+            "solar": {
+                "icon": self.get_plugin_dir(f'icons/solarhaus.png'),
+                "max_power": replace_decimals(str(round(solarMaxPower/1000, 1))) + " kWp",
+                "production_today": replace_decimals(str(round(solarProductionToday, 1))) + " kWh",
+                "current_power": replace_decimals(str(round(solarCurrentPower, 0))) + " W"
+            },
+            
+            #"solar_house_png": self.get_plugin_dir(f'icons/solarhaus.png'),
             "power_plant_png": self.get_plugin_dir(f'icons/strommast.png'),
             "euro_png": self.get_plugin_dir(f'icons/euro.png'),
-            "battery_level": str(batteryLevel) + " %",
-            "battery_capacity": "10 kWh",
-            "consumption_today": str(consumptionToday) + " kWh",
-            "production_today": str(productionToday),
-            "current_power": str(currentPower),
-            "current_date": {"week_day": "Dienstag,",
+            
+            "consumption_today": replace_decimals(str(consumptionToday)) + " kWh",
+            "current_power": str(currentSolarPower),
+            "current_date": {"week_day": "Dienstag",
                              "day": "14.",
                              "month": "Oktober",
                              "time" : "12:00",
-                             "am_pm" : "UHR"
-                            }
+                             "am_pm" : "Uhr"
+                            },
+            "pv_production_max_value": 600,
+            "pv_production_values_shown": 14,
+            "pv_production_data": [10,10,10,10,10,10,10,100,150,170,210,500,600,50,100,10,10,10,10,10,10,10,10,10]
         }
 
         return data
