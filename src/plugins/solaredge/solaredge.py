9e7c2354f971a73dd1a3f1d07a0bfef99aa5dfe9
nx74205 <kl.volmer@gmx.de>
2025-11-01 15:21:49 +0000

    progress

--- src/plugins/solaredge/solaredge.py
+++ src/plugins/solaredge/solaredge.py
@@ -1,5 +1,6 @@
 from plugins.base_plugin.base_plugin import BasePlugin
 from PIL import Image
+import locale
 import os
 import requests
 import logging
@@ -47,12 +48,10 @@ class Solaredge(BasePlugin):
 
     def parse_solar_data(self):
 
-        DECIMAL_SIGN = ","
-
-        def replace_decimals(s1: str) -> str:
-            if not isinstance(s1, str) :
-                return s1
-            return s1.replace(".", DECIMAL_SIGN)
+        descimalSign = ","
+        locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
+        amPm = "Uhr"
+        currencySymbol = "€"
 
         cdt = datetime.now()
 
@@ -64,12 +63,26 @@ class Solaredge(BasePlugin):
         solarProductionToday = 6.82
         solarCurrentPower = 2300
 
+        dapCurrentPrice = 0.305
+        dapNextPrice = 0.295
+
         consumptionToday = 4.5
-        
-        currentSolarPower = 2500
+
+        renewableEnergyPercentage = 75
+        renewableDescription = "Anteil EEG"
+
+        chartMaxValue = 600
+        chartValuesShown = 14
+        chartData = [10,10,10,10,10,10,10,100,150,170,210,500,600,50,10,10,10,10,10,10,10,10,10,10]
+
+        def replace_decimals(s1: str) -> str:
+            if not isinstance(s1, str) :
+                return s1
+            return s1.replace(".", descimalSign)            
 
         data = {
             "solaredge_png": self.get_plugin_dir(f'icons/solaredge.png'),
+            "star_png": self.get_plugin_dir(f'icons/star.png'),
             "battery": {
                 "icon": self.get_plugin_dir(f'icons/battery-' + f'{int(round(batteryLevel / 10) * 10)}' + '.png'),
                 "level": str(batteryLevel) + " %",
@@ -82,23 +95,35 @@ class Solaredge(BasePlugin):
                 "production_today": replace_decimals(str(round(solarProductionToday, 1))) + " kWh",
                 "current_power": replace_decimals(str(round(solarCurrentPower, 0))) + " W"
             },
-            
-            #"solar_house_png": self.get_plugin_dir(f'icons/solarhaus.png'),
-            "power_plant_png": self.get_plugin_dir(f'icons/strommast.png'),
-            "euro_png": self.get_plugin_dir(f'icons/euro.png'),
-            
-            "consumption_today": replace_decimals(str(consumptionToday)) + " kWh",
-            "current_power": str(currentSolarPower),
-            "current_date": {"week_day": "Dienstag",
-                             "day": "14.",
-                             "month": "Oktober",
-                             "time" : "12:00",
-                             "am_pm" : "Uhr"
+            "dap": {    # DAP = Stock "Day Ahead Price" per kWh
+                "icon": self.get_plugin_dir(f'icons/euro.png'),
+                "currency_symbol": currencySymbol,
+                "current_time": "11:00",
+                "current_price": replace_decimals(f"{dapCurrentPrice:+}") + " " + currencySymbol,
+                "next_time": "11:15",
+                "next_price": replace_decimals(f"{dapNextPrice:+}") + " " + currencySymbol
+            },
+            "power_plant": {
+                "icon": self.get_plugin_dir(f'icons/strommast.png'),
+                "consumption_today": replace_decimals(str(consumptionToday)) + " kWh"
+            },
+            "current_date": {"week_day": cdt.strftime('%A'),
+                             "day": cdt.strftime('%d'),
+                             "month": cdt.strftime('%B'),
+                             "time" : cdt.strftime('%H:%M'),
+                             "am_pm" : amPm
                             },
-            "pv_production_max_value": 600,
-            "pv_production_values_shown": 14,
-            "pv_production_data": [10,10,10,10,10,10,10,100,150,170,210,500,600,50,100,10,10,10,10,10,10,10,10,10]
+            "chart": {
+                "max_value": chartMaxValue,
+                "values_shown": chartValuesShown,
+                "data": chartData
+            },
+            "renewable": {
+                "icon": self.get_plugin_dir(f'icons/leaf.png'),
+                "description": renewableDescription,
+                "percentage": str(round(renewableEnergyPercentage, 0)) + " %"
+            }
         }
 
         return data
-
+    
